cd5a4ce4c59a7a4f677150b08100de2aced7dab8
Oleksandr Sydorskyi <88611124+oleksandr-sydorskyi@users.noreply.github.com>
2025-03-18 23:24:00 +0200

    Removed getAllBooksByAuthor method and simplified @RequestMapping annotation

--- src/main/java/mate/academy/bookstore/controller/BookController.java
+++ src/main/java/mate/academy/bookstore/controller/BookController.java
@@ -10,12 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/books")
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
@@ -27,11 +26,6 @@ public class BookController {
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
-    }
-
-    @GetMapping("/by-author")
-    public List<BookDto> getAllBooksByAuthor(@RequestParam String author) {
-        return bookService.getAllBooksByAuthor(author);
     }
 
     @PostMapping
