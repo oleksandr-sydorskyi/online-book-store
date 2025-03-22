51cadadd2ace0baf58501704baab667df4ef5d72
Oleksandr Sydorskyi <88611124+oleksandr-sydorskyi@users.noreply.github.com>
2025-03-22 12:47:23 +0200

    feat: Implement Data JPA, Liquibase, soft delete, and book update/delete endpoints

--- src/main/java/mate/academy/bookstore/repository/BookRepository.java
+++ src/main/java/mate/academy/bookstore/repository/BookRepository.java
@@ -1,15 +1,8 @@
 package mate.academy.bookstore.repository;
 
-import java.util.List;
-import java.util.Optional;
 import mate.academy.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository {
-
-    Book addBook(Book book);
-
-    List<Book> getAllBooks();
-
-    Optional<Book> getBookById(Long id);
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
 
